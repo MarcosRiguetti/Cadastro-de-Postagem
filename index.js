8e13b478acdb66c7a7cee6c37b9e23f62b739ce1
MarcosRiguetti <marcosriguettisoares@outlook.com>
2021-05-19 20:05:42 -0300

    Terceiro commit

--- index.js
+++ index.js
@@ -8,14 +8,20 @@
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handleabrs')
     //Conexão com o banco de dados
+        const Sequelize = require('sequelize')
         const sequileze = new Sequelize('test', 'root', 'MarcoS00', {
             host: "localhost",
             dialect: 'mysql'
         })
+        sequileze.authenticate().then( () => {
+            console.log("Conectado com sucesso!")
+        }).catch(() => {
+            console.log("Falha ao conectar: " + erro)
+        })
 
 // Outros
     // http
-        const  PORT  =  8081
-            app . ouvir ( PORT ,  ( )  =>  {
-                console . log ( "Servidor rodando!" )
+        const PORT = 8081
+            app.ouvir( PORT , ()  => {
+                console.log ("Servidor rodando!")
             } )
---
+++ test.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('test', 'root', 'MarcoS00', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+// const Postagem =  sequelize.define('postagens', {
+//     titulo: {
+//         type: Sequelize.STRING
+//     },
+//     conteudo: {
+//         type: Sequelize.TEXT
+//     }
+// })
+
+//Postagem.sync({force: true})
+
+// const Usuario = sequelize.define('usuarios', {
+//     nome: {
+//         type:Sequelize.STRING
+//     },
+//     sobrenome: {
+//         type:Sequelize.STRING
+//     },
+//     idade: {
+//         type:Sequelize.INTEGER
+//     },
+//     email: {
+//         type:Sequelize.STRING
+//     }
+// })
+
+// Usuario.sync({force: true})
+
+// postagem.create({
+//     titulo: "",
+//     conteudo: ""
+// })
